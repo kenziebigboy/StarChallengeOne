13ffdd3171d22f566429572aaa26970a07a07b92
kenziebigboy <michael.collier@mykenzie.snhu.edu>
2022-11-11 08:20:15 -0700

    initial commit

---
+++ src/com/mastercoding/Main.java
@@ -0,0 +1,34 @@
+package com.mastercoding;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // get started read the README file
+        // there is a patters file with images of all the patters in it
+
+        // code format
+        // use 2 for loop like below
+
+        // You can use System.out.print("*"); to print *
+        // and you can use System.out.print(" "); for space
+        // you can use anything we learned: if-else-then, switch, int counter
+        // thing you can not do is System.out.print("*   *   *  **");
+        // you need to generate the patterns
+
+        int maxrows     = 14;
+        int maxColoumns = 14;
+
+        for(int rows = 0; rows < maxrows; rows++){
+            for(int column = 0; column < maxColoumns; column++){
+                // put your logic here
+                // remember to use print and not println
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
